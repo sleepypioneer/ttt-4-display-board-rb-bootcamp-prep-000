64487e5b7be56b56221ccc75fc9a952ea582f98d
Jessica Greene <jessica0greene@gmail.com>
2017-10-28 20:51:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board (board)
-  row1 = "#{board[0]}|#{board[1]}|#{board[2]}"
+  row1 = "#{board[0]} | #{board[1]} | #{board[2]}"
   row2 = "#{board[3]}|#{board[4]}|#{board[5]}"
   row3 = "#{board[6]}|#{board[7]}|#{board[8]}"
   line = ["-----------"]
